3d362dc957c5d295808e574fb89db02c2c64bcac
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-09-16 20:04:18 -0300

    feat: add documentation crud method

---
+++ course-manage-api/app/Controllers/Http/DocumentationsController.ts
@@ -0,0 +1,16 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Documentation from 'App/Models/Documentation'
+import { STUDENT_ID } from 'App/Utils/constants'
+import DocumentationValidator from 'App/Validators/DocumentationValidator'
+
+export default class DocumentationsController {
+  public async update({ request, response }: HttpContextContract) {
+    const id: number = request.param(STUDENT_ID)
+    const body = await request.validate(DocumentationValidator)
+
+    const documentation = await Documentation.query().where('student_id', id).firstOrFail()
+    await documentation.merge(body).save()
+
+    response.ok(documentation)
+  }
+}
--- course-manage-api/app/Controllers/Http/StudentsController.ts
+++ course-manage-api/app/Controllers/Http/StudentsController.ts
@@ -1,3 +1,5 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Documentation from 'App/Models/Documentation'
 import Student from 'App/Models/Student'
 import { STUDENT_ID } from 'App/Utils/constants'
 import StudentValidator from 'App/Validators/StudentValidator'
@@ -6,5 +8,19 @@ import CrudController from './CrudController'
 export default class StudentsController extends CrudController {
   constructor() {
     super(STUDENT_ID, Student, StudentValidator)
+  }
+
+  public async show({ request, response }: HttpContextContract): Promise<void> {
+    const id: number = request.param(this.idName)
+
+    const row = await Student.query().where('id', id).preload('documentation').firstOrFail()
+
+    response.ok(row)
+  }
+
+  public async store(ctx: HttpContextContract): Promise<any> {
+    const student: Student = await super.store(ctx)
+
+    await Documentation.create({ studentId: student.id })
   }
 }
