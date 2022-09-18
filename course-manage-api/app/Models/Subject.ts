238cdaf62ca7238aeefdeee2665b038a41d36be5
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-09-18 17:58:51 -0300

    feat: define subject model and migration

---
+++ course-manage-api/app/Models/Subject.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Subject extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public status: boolean
+
+  @column.dateTime({ autoCreate: true, serializeAs: null })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public updatedAt: DateTime
+}
