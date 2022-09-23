80f97e528da3f8785585c2ec37a49a88925e8ef2
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-09-22 21:49:58 -0300

    fix: index pagination and added save counts

--- course-manage-api/app/Controllers/Http/AttendancesController.ts
+++ course-manage-api/app/Controllers/Http/AttendancesController.ts
@@ -1,24 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Attendance from 'App/Models/Attendance'
-import Course from 'App/Models/Course'
 import { ATTENDANCE_ID, COURSE_ID, DEFAULT_PAGE, DEFAULT_SIZE } from 'App/Utils/constants'
 import PaginationValidator from 'App/Validators/PaginationValidator'
 import AttendanceValidator from 'App/Validators/AttendanceValidator'
+import StudentCourse from 'App/Models/StudentCourse'
 
 export default class AttendancesController {
   public async index({ request, response }: HttpContextContract): Promise<void> {
     const courseId: number = request.param(COURSE_ID)
     const { page = DEFAULT_PAGE, size = DEFAULT_SIZE } = await request.validate(PaginationValidator)
 
-    const course = await Course.query()
-      .where('id', courseId)
-      .preload('attendaces', (query) => {
-        query.paginate(page, size)
-      })
-      .withCount('attendaces')
-      .firstOrFail()
+    const attendances = await Attendance.query().where('course_id', courseId).paginate(page, size)
 
-    response.ok({ total: course.$extras.attendances_count, result: course.attendaces })
+    response.ok({ total: attendances.total, result: attendances.all() })
   }
 
   public async store({ request, response }: HttpContextContract): Promise<void> {
@@ -27,9 +21,20 @@ export default class AttendancesController {
     const { students, date } = await request.validate(AttendanceValidator)
 
     const setStudents = [...new Set(students)]
-    const attendance = await Attendance.create({ courseId, date, record: setStudents })
+    const attendance = await Attendance.create({ courseId, date, record: { students: setStudents } })
 
     response.ok(attendance)
+
+    const studentCourse = await StudentCourse.query().where('course_id', courseId)
+    studentCourse.forEach(async (relationship) => {
+      if (setStudents.includes(relationship.studentId)) {
+        relationship.attendanceCount++
+      } else {
+        relationship.absenceCount++
+      }
+      relationship.classCount++
+      await relationship.save()
+    })
   }
 
   public async destroy({ request, response }: HttpContextContract): Promise<void> {
@@ -37,8 +42,19 @@ export default class AttendancesController {
     const attendanceId: number = request.param(ATTENDANCE_ID)
     const attendance = await Attendance.query()
       .where('course_id', courseId)
-      .andWhere('attendance_id', attendanceId)
+      .andWhere('id', attendanceId)
       .firstOrFail()
+
+    const studentCourse = await StudentCourse.query().where('course_id', courseId)
+    studentCourse.forEach(async (relationship) => {
+      if (attendance.record.students.includes(relationship.studentId)) {
+        relationship.attendanceCount--
+      } else {
+        relationship.absenceCount--
+      }
+      relationship.classCount--
+      await relationship.save()
+    })
 
     await attendance.delete()
 
