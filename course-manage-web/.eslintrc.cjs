24283200cdee8ffb98ac715cbcc953f95c423f8c
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-09-26 17:24:05 -0300

    elint config

---
+++ course-manage-web/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    indent: [2],
+    'space-before-function-paren': 'off'
+  }
+}
